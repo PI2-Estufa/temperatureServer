deaae932ddbcd5c01ce30673d5a1fe312d9c32f8
leomeister <leomeister22@gmail.com>
2018-04-29 21:05:34 -0300

    Define docker configurations

--- server.py
+++ server.py
@@ -1,1 +1,10 @@
-print('hello world')
+from nameko.rpc import rpc
+
+class TemperatureServer():
+    name = 'temperature_server'
+
+    @rpc
+    def receiveTemperature(self, temperature):
+        print(temperature)
+
+        return True
