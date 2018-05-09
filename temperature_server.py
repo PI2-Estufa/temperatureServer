dd0a69110d7ecdaf96f62c9513e4598a0bbd2fed
Marcelo Oliveira <martins.oliveira.mo@gmail.com>
2018-05-09 17:09:05 -0300

    Create a queue to publish the temperature
    Signed-off-by: Marcelo Oliveira <martins.oliveira.mo@gmail.com>

--- temperature_server.py
+++ temperature_server.py
@@ -1,10 +1,18 @@
 from nameko.rpc import rpc
+from nameko.messaging import Publisher
+from kombu import Exchange, Queue
+
+
+exchange = Exchange("main", "direct", durable=True)
+queue = Queue("temperature_queue", exchange=exchange)
+
 
 class TemperatureServer():
-    name = 'temperature_server'
+    name = "temperature_server"
+    publish = Publisher(exchange=exchange, queue=queue)
 
     @rpc
     def receive_temperature(self, temperature):
         print(temperature)
-
-        return True
+        self.publish(temperature)
+        return temperature
