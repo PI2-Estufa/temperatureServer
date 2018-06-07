1cf9b8423daf7d7b868fc06369dadfc913403171
Lucas Amoêdo <lucas.advc@gmail.com>
2018-06-07 16:11:34 -0300

    Remove queues and round value to one decimal value

--- temperature_server.py
+++ temperature_server.py
@@ -1,23 +1,17 @@
 from nameko.rpc import rpc
-from nameko.messaging import Publisher
-from kombu import Exchange, Queue
 import db
 from db import Temperature
-
-exchange = Exchange("temperature", "direct", durable=True)
-queue = Queue("temperature_queue", exchange=exchange)
 
 
 class TemperatureServer():
     name = "temperature_server"
-    publish = Publisher(exchange=exchange, queue=queue)
 
     @rpc
     def receive_temperature(self, temperature):
+        temperature = round(temperature, 1)
         t = Temperature()
         t.value = temperature
         t.unit = ('C')
         db.session.add(t)
         db.session.commit()
-        self.publish(temperature)
         return temperature
